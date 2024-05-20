308ceb5456cdb831b0ab2c80bc403b84e4f014a1
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-20 11:40:58 +0300

    Initial Commit

---
+++ VolumeControl/VolumeControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  VolumeControlApp.swift
+//  VolumeControl
+//
+//  Created by admin on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct VolumeControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
