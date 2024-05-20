cf62af53ca55013ba2835f6c7d34947eb3506e0d
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-20 21:12:55 +0300

    Setup custom control volume

--- VolumeControl/ContentView.swift
+++ VolumeControl/ContentView.swift
@@ -8,14 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var percentage: Float = 50
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Image("Image")
+                .resizable()
+                .scaledToFill()
+                .edgesIgnoringSafeArea(.all)
+                .blur(radius: 3)
+            CustomControl(percentage: $percentage)
+                .accentColor(.white)
+                .frame(width: 275, height: 75)
+                .rotationEffect(.degrees(-90))
         }
-        .padding()
+    }
+}
+
+struct CustomControl: View {
+    @Binding var percentage: Float // or some value binded
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle()
+                    .background(.ultraThinMaterial)
+                Rectangle()
+                    .foregroundColor(.accentColor)
+                    .frame(width: geometry.size.width * CGFloat(self.percentage / 100))
+            }
+            .cornerRadius(20)
+            .gesture(DragGesture(minimumDistance: 0)
+                .onChanged({ value in
+                    // TODO: - maybe use other logic here
+                    self.percentage = min(max(0, Float(value.location.x / geometry.size.width * 100)), 100)
+                }))
+        }
     }
 }
 
